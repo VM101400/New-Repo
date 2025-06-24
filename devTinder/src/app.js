a65eb5a1ba6fab5a2c5ef158bae96490bd3e33ce
Prakash <prakashmaddi325@gmail.com>
2025-06-24 23:48:17 +0530

    testing routes

--- devTinder/src/app.js
+++ devTinder/src/app.js
@@ -2,29 +2,35 @@ const express = require("express");
 
 const app = express();
 
-// app.use("/hello/bro",(req, res) => {
-//     res.send("Hello brother..!");
-// });
+/*
 
-// app.use("/hello/user",(req, res) => {
-//     res.send("Hello, User..!");
-// });
+app.use("/hello/bro",(req, res) => {
+    res.send("Hello brother..!");
+});
+
+app.use("/hello/user",(req, res) => {
+    res.send("Hello, User..!");
+});
   
-// app.use("/hello",(req, res) => {
-//     res.send("Hello Hello Hello..!");
-// });
+app.use("/hello",(req, res) => {
+    res.send("Hello Hello Hello..!");
+});
 
-// app.use("/test/userdata",(req, res) => {
-//     res.send("User data..!");
-// });
+app.use("/test/userdata",(req, res) => {
+    res.send("User data..!");
+});
 
-// app.use("/test",(req, res) => {
-//     res.send("Hello from the server..!");
-// });
+app.use("/test",(req, res) => {
+    res.send("Hello from the server..!");
+});
 
-// app.use("/",(req, res) => {
-//     res.send("Hello from the Dashboard..!");
-// });
+app.use("/",(req, res) => {
+    res.send("Hello from the Dashboard..!");
+});
+
+*/
+
+/*
 
 // This will only handle GET call to /user
 app.get("/user", (req, res) => {
@@ -43,6 +49,13 @@ app.delete("/user", (req, res) => {
 // This will match all the HTTP method API calls to /test
 app.use("/test",(req, res) => {
     res.send("Hello from the server..!");
+});
+
+*/
+
+app.get("/user/:userid/:name/:password", (req, res) =>{
+    console.log(req.params);   //req.query
+    res.send({firstname: "Sainath", lastname: "Maddi"});
 });
 
 app.listen(8888, () => {
