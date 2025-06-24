52c6c4fe2f23c59b0815c25c82a63ae0b8d52218
Prakash <prakashmaddi325@gmail.com>
2025-06-24 12:07:31 +0530

    devtinder prj

---
+++ devTinder/src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/",(req, res) => {
+    res.send("Hello from the Dashboard..!");
+});
+
+app.use("/hello",(req, res) => {
+    res.send("Hello Hello Hello..!");
+});
+
+app.use("/test",(req, res) => {
+    res.send("Hello from the server..!");
+});
+app.listen(8888, () => {
+    console.log("Server is running successfully on port 8888..!");
+});
