475af834db44f32fe23069ea28a1e84f5714c726
Prakash <prakashmaddi325@gmail.com>
2025-06-24 12:14:57 +0530

    small change

--- devTinder/src/app.js
+++ devTinder/src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.use("/",(req, res) => {
+app.use("/dashboard",(req, res) => {
     res.send("Hello from the Dashboard..!");
 });
 
