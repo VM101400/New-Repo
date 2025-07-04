96216011f106dc893d45d229393803503a470f39
Prakash <prakashmaddi325@gmail.com>
2025-07-04 19:15:10 +0530

    schema validations

--- devTinder/src/app.js
+++ devTinder/src/app.js
@@ -198,12 +198,12 @@ app.patch("/user", async (req, res) =>{
     const userId = req.body.userId;
     const data = req.body;
     try{
-        const user = await User.findByIdAndUpdate({_id: userId}, data, {returnDocument: "after"});
+        const user = await User.findByIdAndUpdate({_id: userId}, data, {returnDocument: "after", runValidators: true});
         console.log(user);
         res.send("User updated Successfully");
     }
     catch(err){
-        res.status(400).send("Something went wrong");
+        res.status(400).send("UPDATE FAILED:" + err.message);
     }
 });
 
--- devTinder/src/models/user.js
+++ devTinder/src/models/user.js
@@ -2,22 +2,50 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 4,
+        maxLength: 50,
     },
     lastName: {
-        type: String
+        type: String,
     },
     emailId: {
-        type: String
+        type: String,
+        lowercase: true,
+        trim: true,
+        required: true,
+        unique: true,
     },
     password: {
-        type: String
+        type: String,
+        required: true,
     },
     age: {
-        type: Number
+        type: Number,
+        min: 18,
     },
     gender: {
-        type: String
+        type: String,
+        validate(value){
+            if(!["male", "female", "others"].includes(value)){
+                throw new Error("Gender data is not valid");
+            }
+        },
     },
+    photoUrl: {
+        type: String,
+        default: "https://www.shutterstock.com/image-vector/vector-design-avatar-dummy-sign-600nw-1290556063.jpg"
+    },
+    about: {
+        type: String,
+        default: "This is a default about of the user!"
+    },
+    skills: {
+        type: [String]
+    }
+}, {
+    timestamps: true,
 });
 module.exports = mongoose.model("User", userSchema);
