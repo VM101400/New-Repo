5386babfbb9dcf66dc61828982c9d3125e33b818
Prakash <prakashmaddi325@gmail.com>
2025-07-04 20:54:28 +0530

    API level data sanitization

--- devTinder/src/app.js
+++ devTinder/src/app.js
@@ -194,10 +194,20 @@ app.delete("/user", async (req, res) =>{
 });
 
 // Update data of the user
-app.patch("/user", async (req, res) =>{
-    const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) =>{
+    const userId = req.params?.userId;
     const data = req.body;
+
     try{
+        const ALLOWED_UPDATES = ["photoUrl", "bout", "gender", "age", "skills"];
+        const isUpdateAllowed = Object.keys(data).every((k) =>
+        ALLOWED_UPDATES.includes(k));
+        if(!isUpdateAllowed){
+            throw new Error("Updates not allowed");
+        }
+        if(data?.skills.length > 10){
+            throw new Error("Skills cannot more than 10");
+        }
         const user = await User.findByIdAndUpdate({_id: userId}, data, {returnDocument: "after", runValidators: true});
         console.log(user);
         res.send("User updated Successfully");
